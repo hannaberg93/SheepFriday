1fdf790402a8384dcd71ac51d94fab29a722cf0e
Christofer Haglund <christofer.haglund@outlook.com>
2019-12-04 16:06:12 +0100

    testing gitignore still

---
+++ public/wp-content/themes/understrap/sidebar-templates/sidebar-menu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<div class="" id="sidebar-menu">
+
+I'm the sdsdsdddddddddd!1
+
+</div><!-- #left-sidebar -->
