b14394fd63dede0d3e520fccc73dd043100b7034
Fabian Olsson <fabtasticdesign@live.se>
2019-12-05 12:13:34 +0100

    footer init

---
+++ public/wp-content/themes/understrap/global-templates/footer.php
@@ -0,0 +1,34 @@
+
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+			<div class="contact-us">
+				<h2 class="text-xs-center">Kontakta oss</h2>
+			<div>
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Lorem ipsumus</h3>
+                        <ul>
+                            <li><a href="#">item 1</a></li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Lorem ipsum</h3>
+                        <ul>
+                            <li><a href="#">item 1</a></li>
+                            <li><a href="#">item 2</a></li>
+                            <li><a href="#">item 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Lorem ipsum</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Sheep Friday © 2020</p>
+            </div>
+        </footer>
+    </div>
