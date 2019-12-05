6fd86269b3933b5194ce209db0adbb60bf4a0f7d
Fabian Olsson <fabtasticdesign@live.se>
2019-12-05 14:45:04 +0100

    uploaded logo, designed the footer better with fa icons and more information

--- public/wp-content/themes/understrap/global-templates/footer.php
+++ public/wp-content/themes/understrap/global-templates/footer.php
@@ -2,31 +2,49 @@
 <div class="footer-dark">
         <footer>
             <div class="container">
-			<div class="contact-us">
-				<h2 class="text-xs-center">Kontakta oss</h2>
+			<div class="d-flex row contact-us">
+				<div class="d-flex col-12 col-md-2 col-lg-2 justify-content-center"><img src="../wp-content/uploads/media/Sheepfriday.svg" class="footer-logo"></div>
+				<div class="d-flex col-12 col-md-3 col-lg-3 justify-content-center"><h2 class="contact-time"></h2>
+				<ul>
+					<h2>Contact Us</h2>
+					<li>Business Days: 10.00-19.00</li>
+					<li>Week Days: 14.00-16.00</li>
+				</ul>
+				</div>
+				<div class="d-flex col-12 col-md-7 col-lg-7 justify-content-center">
+					<ul>
+						<li><h2 class="contact-phone"><i class="fa fa-mobile"></i> 07000787</h2></li>
+						<li><h2><a href="linkuslinkus/somethin" class="contact-mail"><i class="fa fa-envelope"></i> mail@sheepfriday.test</a></h2></li>
+					</ul>
+				</div>
+
 			<div>
-                <div class="row">
+				<hr>
+                <div class="row mt-4">
                     <div class="col-sm-6 col-md-3 item">
-                        <h3>Lorem ipsumus</h3>
+                        <h3>The Company</h3>
                         <ul>
-                            <li><a href="#">item 1</a></li>
-                            <li><a href="#">item 2</a></li>
-                            <li><a href="#">item 3</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Our Products</a></li>
+                            <li><a href="#">Payments</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-6 col-md-3 item">
-                        <h3>Lorem ipsum</h3>
+                        <h3>Location</h3>
                         <ul>
-                            <li><a href="#">item 1</a></li>
-                            <li><a href="#">item 2</a></li>
-                            <li><a href="#">item 3</a></li>
+                            <li><a href="#">Drottninggatan 12</a></li>
+                            <li><a href="#">211 21 Malmö</a></li>
+                            <li><a href="#">Skåne Sweden</a></li>
                         </ul>
                     </div>
                     <div class="col-md-6 item text">
                         <h3>Lorem ipsum</h3>
-                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+						<p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
                     </div>
-                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+					<div class="col item social">
+					<h2>Get our latest news</h2>
+					<a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a>
+					</div>
                 </div>
                 <p class="copyright">Sheep Friday © 2020</p>
             </div>
