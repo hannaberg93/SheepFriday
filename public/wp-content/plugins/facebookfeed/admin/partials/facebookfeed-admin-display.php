956e479ae673ed30221a0e2bdecd1fc3765b7756
Fabian Olsson <fabtasticdesign@live.se>
2019-12-09 11:21:32 +0100

    plugin boilerplate init

---
+++ public/wp-content/plugins/facebookfeed/admin/partials/facebookfeed-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       www.fabianolsson.se
+ * @since      1.0.0
+ *
+ * @package    Facebookfeed
+ * @subpackage Facebookfeed/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ public/wp-content/plugins/facebookfeed/includes/class-facebookfeed-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       www.fabianolsson.se
+ * @since      1.0.0
+ *
+ * @package    Facebookfeed
+ * @subpackage Facebookfeed/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Facebookfeed
+ * @subpackage Facebookfeed/includes
+ * @author     Fabian Olsson <fab@fab.com>
+ */
+class Facebookfeed_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
