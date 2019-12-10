9a1e497306fec4ede7275e8ac4dbd7752e99a6a7
Albann99 <46557030+Albann99@users.noreply.github.com>
2019-12-10 15:19:49 +0100

    changed currency symol form kr to :-

--- public/wp-content/themes/understrap/functions.php
+++ public/wp-content/themes/understrap/functions.php
@@ -63,3 +63,15 @@ function sheep_get_menu_items() {
 	echo '</a>';
     }
 }
+
+/**
+ * Change a currency symbol
+ */
+add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
+
+function change_existing_currency_symbol( $currency_symbol, $currency ) {
+     switch( $currency ) {
+          case 'SEK': $currency_symbol = ':-'; break;
+     }
+     return $currency_symbol;
+}
