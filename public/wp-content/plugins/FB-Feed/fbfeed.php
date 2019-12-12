d1274fafca39e22f4b78ce038de93352e582d85e
Fabian Olsson <fabtasticdesign@live.se>
2019-12-12 14:13:42 +0100

    exceptions fixed, also added some styling to the feed items

--- public/wp-content/plugins/FB-Feed/fbfeed.php
+++ public/wp-content/plugins/FB-Feed/fbfeed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Facebook Feed
  * Plugin URI:  
- * Description: This plugin for displaying some StarWars trivia.
+ * Description: Shortcode for retrieving facebook posts.
  * Version:     0.1
  * Author:      Fab
  * Author URI:  https://pluginseverywhere.noooo
@@ -12,22 +12,15 @@
  * Domain Path: /languages
  */
 
- // Connect to the FB API
-//  $fb = new Facebook\Facebook([
-//     'app_id' => FB_APP_ID,
-//     'app_secret' => FB_APP_SECRET,
-//     'default_graph_version' => 'v3.1'
-// ]);
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/facebook/graph-sdk/src/Facebook/autoload.php';
 include 'sf-feed-tokens.php';
-
 function output_FB_feed( $atts ) {
 
     $a = shortcode_atts( array(
        'app_id' => FB_APP_ID,
        'app_secret' => FB_APP_SECRET,
-       'access_token' => FB_ACCESS_TOKEN,
-       'feed_limit' => ''
+       'access_token' => false , //FB_ACCESS_TOKEN
+       'feed_limit' => 2
     ), $atts );
 
     $fb = new Facebook\Facebook([
@@ -38,36 +31,26 @@ function output_FB_feed( $atts ) {
 
     try {
         $response = $fb->get(
- 
-        '/sheepfriday/?fields=feed{message,attachments}',
+        '/sheepfriday/?fields=feed.limit('. $a['feed_limit'] .'){message,attachments}',
         FB_ACCESS_TOKEN
         );
-    } catch(FacebookExceptionsFacebookResponseException $e) {
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
         echo 'Graph returned an error: ' . $e->getMessage();
         exit;
-    } catch(FacebookExceptionsFacebookSDKException $e) {
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
         echo 'Facebook SDK returned an error: ' . $e->getMessage();
         exit;
     }
     $graphNode = $response->getGraphNode();
     $array = json_decode($graphNode,true);
     $feed = $array['feed'];
-    //Scan thru the array feed retrieved from the json_decode
     foreach ($feed as $item) {
-        //Check if $item is an array
         if ($item["message"] && $item['attachments']){
-            // var_dump($item['message']);
-            // var_dump($item['attachments'][0]['media']['image']['src']);
-            $content .='<div>' . $item["message"] . '</div>';
-            $content .='<img src="' . $item['attachments'][0]['media']['image']['src'] . '"class="post-img" alt="Postpics" height="100%" width="50vw">';
-                //     $content .='<div>' . $item["message"] . '</div>';
-                // }
-    
-        // $content = '<div id=#'.$item["id"].'>';
-        // $content .='</div>';
+            $content .='<div class="card p-2 mt-2 d-flex w-3 text-center shadow bg-white rounded">' . $item["message"];
+            $content .='<img src="' . $item['attachments'][0]['media']['image']['src'] . '"class="img-responsive mt-4 w-75 mx-auto" alt="Postpics"></div>';
         }
         elseif ($item["message"] && !$item['attachments']) {
-            $content .='<div>' . $item["message"] . '</div>';
+            $content .='<div class="card p-2 mt-2 shadow bg-white rounded">' . $item["message"] . '</div>';
         }
         else {
             
