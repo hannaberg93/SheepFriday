bcbbdb9dab3f763944277570bc4f385cd2319e03
Fabian Olsson <fabtasticdesign@live.se>
2019-12-13 14:19:34 +0100

    fix to avoid hard plugin crashes also fix to render out more than one image

--- public/wp-content/plugins/FB-Feed/fbfeed.php
+++ public/wp-content/plugins/FB-Feed/fbfeed.php
@@ -19,7 +19,7 @@ function output_FB_feed( $atts ) {
     $a = shortcode_atts( array(
        'app_id' => FB_APP_ID,
        'app_secret' => FB_APP_SECRET,
-       'access_token' => false , //FB_ACCESS_TOKEN
+       'access_token' => false, //FB_ACCESS_TOKEN
        'feed_limit' => 2
     ), $atts );
 
@@ -31,26 +31,59 @@ function output_FB_feed( $atts ) {
 
     try {
         $response = $fb->get(
-        '/sheepfriday/?fields=feed.limit('. $a['feed_limit'] .'){message,attachments},picture,name',
-        FB_ACCESS_TOKEN
+        '/sheepfriday/?fields=feed.limit('. $a['feed_limit'] .'){message,attachments,created_time{subattachments,media}},picture,name',
+        $a['access_token']//FB_ACCESS_TOKEN
         );
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
         echo 'Graph returned an error: ' . $e->getMessage();
+        //if it fails deactivate the plugin to avoid problems with pages where shortcode is active
+        deactivate_plugins( plugin_basename( __FILE__ ) );
         exit;
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        deactivate_plugins( plugin_basename( __FILE__ ) );
+        //if it fails deactivate the plugin to avoid problems with pages where shortcode is active
         echo 'Facebook SDK returned an error: ' . $e->getMessage();
         exit;
     }
     $graphNode = $response->getGraphNode();
     $array = json_decode($graphNode,true);
     $feed = $array['feed'];
+
+    
     foreach ($feed as $item) {
+        //check for an image
         if ($item["message"] && $item['attachments']){
-            $content .='<div class="card p-2 mt-2 shadow bg-white rounded"><div class="d-inline"><img src="'. $array['picture']['url'] .'"><h2 class="d-inline ml-2">'.$array['name'].'</h2><hr></div><div><p>' . $item["message"] . '</p></div>';
-            $content .='<img src="' . $item['attachments'][0]['media']['image']['src'] . '"class="img-responsive mt-4 mb-4 w-75 mx-auto" alt="Postpics"></div>';
+            $content .='<div class="card p-2 mt-2 shadow bg-white rounded">
+                            <div class="d-inline">
+                                <img src="'. $array['picture']['url'] .'"><h2 class="d-inline ml-2">'.$array['name'].'</h2><hr>
+                            </div>
+                        <div>
+                            <p>' . $item["message"] . '</p>
+                            </div>';
+            //Check for several images
+            if ($item['attachments'][0]['subattachments']) {
+                foreach ($item['attachments'][0]['subattachments'] as $img){
+                    var_dump($img['media']['image']['src']);
+                    $content .= '<img src="' . $img['media']['image']['src'] . '"class="img-responsive mt-4 mb-4 w-45 mx-auto" alt="Postpics">';
+                }  
+                    $content .= '<p class="font-italic">' . date('Y jS  F h:i', strtotime($item['created_time']['date'])) . '</p>
+                </div>';
+            }
+            else{
+                $content .= '<img src="' . $item['attachments'][0]['media']['image']['src'] . '"class="img-responsive mt-4 mb-4 w-75 mx-auto" alt="Postpics"></div>';
+            }
         }
+        //if no attachments are set render out only the message
         elseif ($item["message"] && !$item['attachments']) {
-            $content .='<div class="card p-2 mt-2 shadow bg-white rounded"><div class="d-inline"><img src="'. $array['picture']['url'] .'"><h2 class="d-inline ml-2">'.$array['name'].'</h2><hr></div><div><p>' . $item["message"] . '</p></div></div>';
+            $content .='<div class="card p-2 mt-2 shadow bg-white rounded">
+                            <div class="d-inline">
+                                <img src="'. $array['picture']['url'] .'"><h2 class="d-inline ml-2">'.$array['name'].'</h2><hr>
+                            </div>
+                            <div>
+                                <p>' . $item["message"] . '</p>
+                                <p class="font-italic">' . date('Y jS  F h:i', strtotime($item['created_time']['date'])) . '</p>
+                            </div>
+                        </div>';
         }
         else {
             
