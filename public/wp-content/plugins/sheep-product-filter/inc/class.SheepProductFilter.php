6efdfc814bcb6198142f8274648fddf1fc174d7b
Hanna <hanna.berg93@gmail.com>
2020-01-01 19:10:01 +0100

    Changed html and logic for filtering options

--- public/wp-content/plugins/sheep-product-filter/inc/class.SheepProductFilter.php
+++ public/wp-content/plugins/sheep-product-filter/inc/class.SheepProductFilter.php
@@ -1,36 +1,65 @@
 <?php
 
-class SheepProductFilter {
-	public function __construct() {		
-          add_action('woocommerce_before_shop_loop', [$this, 'init'], 0);
-     }
+if (!class_exists('SheepProductFilter')) {
 
-     public function init() {	
-        if(is_shop() || is_product_category()){ 
-            //remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
+    class SheepProductFilter {
+        public function __construct() {	
+            add_action('woocommerce_before_shop_loop', [$this, 'startRow'], 0);
+            add_action('woocommerce_before_shop_loop', [$this, 'init'], 0);
+            add_action('woocommerce_before_shop_loop', [$this, 'endRow'], 0);
+        }
 
-            $taxonomies = get_terms([
-                'taxonomy' => 'product_cat',
-                'hide_empty' => false
-            ]);
+        public function startRow() {
+            echo 
+            '<div class="row mt-2 mb-2">
+                <div class="custom-order col-12 d-flex align-items-center">';
+        }
 
-            $brands = get_terms([
-                'taxonomy' => 'pa_brand',
-                'hide_empty' => false
-            ]);
+        public function endRow() {
+            echo 
+                '
+                    <a href="' . get_permalink(woocommerce_get_page_id('shop')) . '" class="btn btn-primary ml-2" id="clearForms">Clear</a>
+                </div>
+            </div>';
+        }
 
-            if(!empty($taxonomies)){ ?>
-                <form class="woocommerce-ordering">
-                    <select class="custom-select filterby" id="selectCategory" onChange="window.location.href=this.value">
-                        <option>Filter by category</option>
-                        <?php foreach($taxonomies as $taxonomy){
-                            if($taxonomy->parent == 0){ ?>
-                                <option value="<?php echo get_term_link($taxonomy->term_id); ?>"><?php echo esc_html($taxonomy->name); ?>
-                                    <?php foreach($taxonomies as $subtaxonomy){
-                                        if($subtaxonomy->parent == $taxonomy->term_id){ ?>
-                                            <option value="<?php echo get_term_link($taxonomy->term_id); ?>"> - <?php echo esc_html($subtaxonomy->name); ?></option>
-                                        <?php }
-                                    } ?>
+        public function init() {	
+            if(is_shop() || is_product_category() || is_tax()) { 
+                remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+                $this->getFormSelect(get_terms([
+                    'taxonomy' => 'product_cat',
+                ]), 'category');
+
+
+                $this->getFormSelect(get_terms([
+                    'taxonomy' => 'pa_brand',
+                ]), 'brand');
+
+                woocommerce_catalog_ordering();
+            }
+        }
+
+        public function getFormSelect($items, $title){
+            if(!empty($items)){ ?>
+                <form class="flex-grow-1">
+                    <select class="custom-select filterby" id="selectFilter" onChange="window.location.href=this.value">
+                        <option disabled value="" label="<?php echo 'Filter by ' . $title; ?>"></option>
+                        <?php foreach($items as $item){
+                            if($item->parent == 0){
+
+                                if($title === 'brand'){ ?>
+                                    <option value="<?php echo get_term_link($item->term_id); ?>"><?php echo esc_html($item->name) . ' (' . $item->count . ')';
+                                }else if($title === 'category'){ ?>
+                                    <option value="<?php echo get_term_link($item->term_id); ?>"><?php echo esc_html($item->name);
+                                }
+
+                                foreach($items as $subitem){
+                                    if($subitem->parent == $item->term_id){ ?>
+                                        <option value="<?php echo get_term_link($subitem->term_id); ?>"> - <?php echo esc_html($subitem->name) . ' (' . $subitem->count . ')'; ?></option>
+                                    <?php }
+                                } ?>
+
                                 </option>
                             <?php } 
                         }?>
