a48948728a7c331f7ca5cef52bd040cf7e22195a
Christofer Haglund <christofer.haglund@outlook.com>
2020-01-05 14:06:14 +0100

    shop now acts as front page with special offers and fb news

--- public/wp-content/themes/understrap/woocommerce/archive-product.php
+++ public/wp-content/themes/understrap/woocommerce/archive-product.php
@@ -25,95 +25,98 @@ get_header( 'shop' ); ?>
   </div>
   <div class='col'>
 
-<?php
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action( 'woocommerce_before_main_content' );
-?>
+    <?php
+    /**
+     * Hook: woocommerce_before_main_content.
+     *
+     * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+     * @hooked woocommerce_breadcrumb - 20
+     * @hooked WC_Structured_Data::generate_website_data() - 30
+     */
+    do_action( 'woocommerce_before_main_content' );
+    ?>
 
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+    <header class="woocommerce-products-header">
+      <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+        <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+      <?php endif; ?>
 
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
-<?php
-if ( woocommerce_product_loop() ) {
+      <h2 class="mt-5 mb-5">Specials & Offers</h2>
+      <?php get_template_part( 'global-templates/special-hero' ); ?>
 
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
+      <?php
+      /**
+       * Hook: woocommerce_archive_description.
+       *
+       * @hooked woocommerce_taxonomy_archive_description - 10
+       * @hooked woocommerce_product_archive_description - 10
+       */
+      do_action( 'woocommerce_archive_description' );
+      ?>
+    </header>
+    <?php
+    if ( woocommerce_product_loop() ) {
 
-	woocommerce_product_loop_start();
+      /**
+       * Hook: woocommerce_before_shop_loop.
+       *
+       * @hooked woocommerce_output_all_notices - 10
+       * @hooked woocommerce_result_count - 20
+       * @hooked woocommerce_catalog_ordering - 30
+       */
+      do_action( 'woocommerce_before_shop_loop' );
 
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-      
-        the_post();
+      woocommerce_product_loop_start();
 
-        /**
-         * Hook: woocommerce_shop_loop.
-         */
-        do_action( 'woocommerce_shop_loop' );
+      if ( wc_get_loop_prop( 'total' ) ) {
+        while ( have_posts() ) {
+          
+            the_post();
 
-        wc_get_template_part( 'content', 'product' );
-		}
-	}
+            /**
+             * Hook: woocommerce_shop_loop.
+             */
+            do_action( 'woocommerce_shop_loop' );
 
-	woocommerce_product_loop_end();
+            wc_get_template_part( 'content', 'product' );
+        }
+      }
 
-  // After products show ads
-  wc_get_template_part('ads');
+      woocommerce_product_loop_end();
 
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
+      // After products show ads
+      wc_get_template_part('ads');
 
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action( 'woocommerce_after_main_content' );
+      /**
+       * Hook: woocommerce_after_shop_loop.
+       *
+       * @hooked woocommerce_pagination - 10
+       */
+      do_action( 'woocommerce_after_shop_loop' );
+    } else {
+      /**
+       * Hook: woocommerce_no_products_found.
+       *
+       * @hooked wc_no_products_found - 10
+       */
+      do_action( 'woocommerce_no_products_found' );
+    }
 
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action( 'woocommerce_sidebar' );
+    /**
+     * Hook: woocommerce_after_main_content.
+     *
+     * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+     */
+    do_action( 'woocommerce_after_main_content' );
 
-?>
+    /**
+     * Hook: woocommerce_sidebar.
+     *
+     * @hooked woocommerce_get_sidebar - 10
+     */
+    do_action( 'woocommerce_sidebar' );
+
+    ?>
 
   </div><!-- end .col -->
 </div><!-- end .row -->
